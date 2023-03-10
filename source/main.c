3d857a714fd6d802b91a896e883be1e152a9ea29
Paul Chambaz <paul.chambazz@gmail.com>
2023-03-10 15:31:18 +0100

    update for git

--- source/main.c
+++ source/main.c
@@ -1,8 +1,6 @@
 /**
- * @file main.c
+ * Choice is a cli alternative to program like dmenu or fzf
  * @author Paul Chambaz
- * @date Sep 02 2022
- * @brief This is the main file of the project
  * @license GPLv2
  */
 
@@ -15,21 +13,21 @@
 void copy_file_to_lines ( FILE *file, char ***str, size_t *len );
 
 void exit_error ( char *message );
+bool is_arg ( const char *val, int argc, char *argv[] );
 
-/**
- * @brief The main function
- * @param argc The number of arguments
- * @param argv The list of arguments
- */
-int
-main ( int argc, char *argv[] )
-{
+int length_nb_b10(int number) {
+  if (number < 10)
+    return 1;
+  return length_nb_b10(number / 10) + 1;
+}
+
+int main(int argc, char *argv[]) {
   char **lines;
   size_t number_lines;
 
   copy_file_to_lines(stdin, &lines, &number_lines);
 
-  while (getchar() != EOF) { }
+  bool number_mode = is_arg("-n", argc, argv);
 
   FILE *tty = fopen("/dev/tty", "r+");
   if (!tty)
@@ -37,16 +35,16 @@ main ( int argc, char *argv[] )
 
   // prints the output
   for (int i = 0; i < number_lines; i++) {
-    fprintf(tty, "%d: %s\n", i + 1, lines[i]);
+    fprintf(tty, "%*d: %s\n", length_nb_b10(number_lines + 1), i + 1, lines[i]);
   }
-  fprintf(tty, "%d: Exit\n", number_lines + 1);
+  fprintf(tty, "%*d: Exit\n", length_nb_b10(number_lines + 1), number_lines + 1);
 
   int choice = 0;
   char buf[16];
   do {
     // gets the answer from user
-    if (argc > 1) {
-      fprintf(tty, "%s (1-%d): ", argv[1], number_lines + 1);
+    if ((number_mode && argc > 2) || (!number_mode && argc > 1)) {
+      fprintf(tty, "%s (1-%d): ", argv[argc - 1], number_lines + 1);
     } else {
       fprintf(tty, "Enter your choice (1-%d): ", number_lines + 1);
     }
@@ -54,8 +52,13 @@ main ( int argc, char *argv[] )
     choice = atoi(buf);
   } while (choice < 1 || choice > number_lines + 1);
 
-  if (choice != number_lines + 1)
-    printf("%s\n", lines[choice - 1]);
+  if (choice != number_lines + 1) {
+    if (number_mode) {
+      printf("%d\n", choice);
+    } else {
+      printf("%s\n", lines[choice - 1]);
+    }
+  }
 
   fclose(tty);
 }
@@ -66,9 +69,7 @@ main ( int argc, char *argv[] )
  * @param lines The address of the lines to copy to
  * @param number_lines The address of the number of lines
  */
-void
-copy_file_to_lines ( FILE *file, char ***lines, size_t *number_lines )
-{
+void copy_file_to_lines(FILE *file, char ***lines, size_t *number_lines) {
   // TODO: this function needs commenting
   char c;
   size_t nb_lines = 1;
@@ -118,10 +119,27 @@ copy_file_to_lines ( FILE *file, char ***lines, size_t *number_lines )
  * @brief Prints an error message before exiting the program
  * @param message The message to print
  */
-void
-exit_error ( char *message )
-{
+void exit_error(char *message) {
   if (message)
     fprintf(stderr, "%s\n", message);
   exit(EXIT_FAILURE);
+}
+
+/* @brief Simple bool function to simplify main
+ * @param i The position of the argument
+ * @param val The argument string to be matched against
+ * @param argc Main's argc
+ * @param argv Main's argv
+ * @return true if the argument exists and matches the value
+ * @return false otherwise
+ */
+bool is_arg (const char *val, int argc, char *argv[])
+{
+  if (argc < 2)
+    return false;
+  for (int i = 1; i < argc; i++) {
+    if (!strcmp(argv[i], val))
+      return true;
+  }
+  return false;
 }
